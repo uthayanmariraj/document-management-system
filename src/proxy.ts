f848bd310fa796c266d6b6a91d5871196da95652
uthayan <uthayanmariraj@gmail.com>
2026-06-27 11:51:49 +0530

    replaced the depracted middleware naming convention and also enforced link safety

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,32 +2,32 @@ import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 import type { NextRequest } from "next/server";
 
-export async function middleware(req: NextRequest) {
-  // 1. Get the session token from the request cookies
+export default async function proxy(req: NextRequest) {
+
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   const { pathname } = req.nextUrl;
 
-  // 2. If the user is logged in, block them from visiting /login or /register
-  // and redirect them to the /dashboard instead.
+
   if (token && (pathname === "/login" || pathname === "/register")) {
     return NextResponse.redirect(new URL("/dashboard", req.url));
   }
 
-  // 3. If the user is NOT logged in, block them from visiting /dashboard, /upload or /api/files
-  // and redirect them to /login instead.
+
   if (!token && (pathname.startsWith("/dashboard") || pathname.startsWith("/api/files") || pathname.startsWith("/upload"))) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
-  // Allow the request to proceed normally
   return NextResponse.next();
 }
 
-// Configure the exact paths this middleware should run on
+
 export const config = {
   matcher: [
+    "/dashboard",
     "/dashboard/:path*",
+    "/api/files",
     "/api/files/:path*",
+    "/upload",
     "/upload/:path*",
     "/login",
     "/register"
